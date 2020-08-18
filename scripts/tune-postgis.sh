4df1759c9cd86b283a046e9f3f3d7cb9476851a8
davidwkelley <49653512+davidwkelley@users.noreply.github.com>
2020-08-18 13:03:39 +0100

    fix scripts for my setup, round 5

--- scripts/tune-postgis.sh
+++ scripts/tune-postgis.sh
@@ -5,3 +5,5 @@ export PGUSER="$POSTGRES_USER"
 
 "${psql[@]}" -c "ALTER SYSTEM SET work_mem='${PG_WORK_MEM:-16MB}';"
 "${psql[@]}" -c "ALTER SYSTEM SET maintenance_work_mem='${PG_MAINTENANCE_WORK_MEM:-256MB}';"
+"${psql[@]}" -c "ALTER SYSTEM SET shared_buffers='${PG_SHARED_BUFFERS:-128MB}';"
+"${psql[@]}" -c "ALTER SYSTEM SET autovacuum='${PG_AUTOVACUUM:OFF}';"
